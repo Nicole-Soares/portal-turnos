e040ccf40ec9f96415739b50688276822ddbf248
Nicole-Soares <nicolesoares918@gmail.com>
2026-02-24 17:14:40 -0300

    Base inicial del proyecto Spring Boot

---
+++ src/main/java/com/clinicapp/portalturnos/PortalTurnosApplication.java
@@ -0,0 +1,13 @@
+package com.clinicapp.portalturnos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalTurnosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalTurnosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicapp/portalturnos/PortalTurnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicapp.portalturnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalTurnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
